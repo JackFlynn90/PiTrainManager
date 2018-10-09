356ecc7466b10a352b256aa1af8199943a35ec1e
Jack <jack90@gmail.com>
2018-10-09 10:09:00 +0000

    Fixed working on python anywhere

--- trains/views.py
+++ trains/views.py
@@ -5,47 +5,50 @@ from .models import Train, Light
 from django.views.decorators.csrf import csrf_exempt
 #import redis
 
-def train_list(request):
-	trains = Train.objects.all()
-	return render(request, 'trains/train_list.html', {'trains': trains})
+try:
+    def train_list(request):
+    	trains = Train.objects.all()
+    	return render(request, 'trains/train_list.html', {'trains': trains})
 
 
-def main_page(request):
-	trains = Train.objects.all()
-	lights = Light.objects.all()
+    def main_page(request):
+    	trains = Train.objects.all()
+    	lights = Light.objects.all()
 
-	return render(request, 'trains/index.html', {'trains': trains, 'lights' : lights})
-	
-@csrf_exempt
-def command_ajax(request):
-	if request.method == 'POST':
-		datatype = request.POST.get('datatype')
-		
-		# r = redis.StrictRedis(host='localhost', port=6379)
-		# p = r.pubsub()
-		
-		
-		if datatype == "command":
-			data = request.POST.get('command')
-			# print("Lighting GOT This: " + data)
-			r.publish('lightCommand', data)
-		elif datatype == "hex":
-			data = request.POST.get('data')
-			# r.publish('lightCommand', data)
-			print("Lighting GOT This Hex value;" + data)
-		return HttpResponse("ok")
+    	return render(request, 'trains/index.html', {'trains': trains, 'lights' : lights})
 
-@csrf_exempt
-def command_ajax_trains(request):
-	if request.method == 'POST':
-		datatype = request.POST.get('datatype')
-		
-		# r = redis.StrictRedis(host='localhost', port=6379)
-		# p = r.pubsub()
-		
-		
-		if datatype == "command":
-			data = request.POST.get('command')
-			print("Trains GOT This: " + data)
-			# r.publish('trainCommand', data)
-		return HttpResponse("ok")
+    @csrf_exempt
+    def command_ajax(request):
+    	if request.method == 'POST':
+    		datatype = request.POST.get('datatype')
+
+    		# r = redis.StrictRedis(host='localhost', port=6379)
+    		# p = r.pubsub()
+
+
+    		if datatype == "command":
+    			data = request.POST.get('command')
+    			# print("Lighting GOT This: " + data)
+    			#r.publish('lightCommand', data)
+    		elif datatype == "hex":
+    			data = request.POST.get('data')
+    			# r.publish('lightCommand', data)
+    			print("Lighting GOT This Hex value;" + data)
+    		return HttpResponse("ok")
+
+    @csrf_exempt
+    def command_ajax_trains(request):
+    	if request.method == 'POST':
+    		datatype = request.POST.get('datatype')
+
+    		# r = redis.StrictRedis(host='localhost', port=6379)
+    		# p = r.pubsub()
+
+
+    		if datatype == "command":
+    			data = request.POST.get('command')
+    			print("Trains GOT This: " + data)
+    			# r.publish('trainCommand', data)
+    		return HttpResponse("ok")
+except OperationalError:
+    pass
